9b6d534d4ad3defdfd1ff1d95515421ec2e5c41d
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2021-06-16 15:27:22 +0800

    d

---
+++ src/App2.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="" class="navbar-brand" title="hello">Ciel's note</a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/program/community">Program</router-link>
+          </li>
+          <li>
+            <router-link to="/web">Web</router-link>
+          </li>
+          <li>
+            <router-link to="/english">English</router-link>
+          </li>
+
+          <li>
+            <router-link to="/financial">Financial</router-link>
+          </li>
+          <li>
+            <router-link to="/history/category">History</router-link>
+          </li>
+          <li>
+            <router-link to="/life">Life</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
+<style>
+@import "style/docs.css";
+@import "style/bootstrap.css";
+@import "style/dashboard.css";
+@import "style/navbar-fixed-top.css";
+@import "style/blog.css";
+</style>
+
