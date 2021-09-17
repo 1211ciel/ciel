57c7b3a4ea9b1a8f029bd6959a69d037add4097d
1211ciel <1211sciel@gmail.com>
2021-09-17 20:02:00 +0800

    'd'

---
+++ mind/sciel_read_site.mm
@@ -0,0 +1,134 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="ciel语言学习阅读网" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1631866913530">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="英语,日语..." POSITION="right" ID="ID_1049985431" CREATED="1631866920849" MODIFIED="1631866944817"/>
+<node TEXT="阅读分类" POSITION="right" ID="ID_395769645" CREATED="1631866945633" MODIFIED="1631866956261">
+<node TEXT="小说" ID="ID_1644965630" CREATED="1631866958624" MODIFIED="1631866963001">
+<node TEXT="章节信息" GLOBALLY_VISIBLE="true" ALIAS="章节属性" ID="ID_1702982091" CREATED="1631867022865" MODIFIED="1631876368340" LINK="#ID_1627524300">
+<attribute_layout NAME_WIDTH="46.5 pt" VALUE_WIDTH="134.25 pt"/>
+<attribute NAME="name" VALUE="1" OBJECT="org.freeplane.features.format.FormattedNumber|1"/>
+<attribute NAME="url" VALUE="#ID_1627524300" OBJECT="java.net.URI|#ID_1627524300"/>
+<node TEXT="关卡数量" ID="ID_969957768" CREATED="1631867048107" MODIFIED="1631867109723"/>
+<node TEXT="章节介绍" ID="ID_1001017156" CREATED="1631867141304" MODIFIED="1631867276615"/>
+<node TEXT="段落信息" ID="ID_1055771721" CREATED="1631867040028" MODIFIED="1631867046022">
+<node TEXT="段落介绍" ID="ID_930297084" CREATED="1631867252254" MODIFIED="1631867280381"/>
+<node TEXT="有多少句话" ID="ID_527596005" CREATED="1631867177969" MODIFIED="1631867184866"/>
+</node>
+</node>
+<node TEXT="关卡数量" ID="ID_1203486715" CREATED="1631867080908" MODIFIED="1631867100593"/>
+<node TEXT="多少单词" ID="ID_1715053763" CREATED="1631867202445" MODIFIED="1631867216062"/>
+<node TEXT="多少句子" ID="ID_514005927" CREATED="1631867221932" MODIFIED="1631867224638"/>
+<node TEXT="description" ID="ID_545170903" CREATED="1631867060352" MODIFIED="1631867161771"/>
+</node>
+<node TEXT="文章" ID="ID_1290279875" CREATED="1631866963324" MODIFIED="1631866967501"/>
+</node>
+<node TEXT="语言翻译与收集" POSITION="right" ID="ID_955885486" CREATED="1631866969909" MODIFIED="1631866980858">
+<node TEXT="说明" ID="ID_115075722" CREATED="1631866983839" MODIFIED="1631866988197"/>
+<node TEXT="规则" ID="ID_483594786" CREATED="1631866988344" MODIFIED="1631866992336"/>
+</node>
+<node TEXT="学习模块" POSITION="right" ID="ID_180697900" CREATED="1631866998900" MODIFIED="1631867001422"/>
+<node TEXT="数据库" POSITION="right" ID="ID_1143278452" CREATED="1631867315493" MODIFIED="1631867317850">
+<node TEXT="小说表" ID="ID_403609079" CREATED="1631867364826" MODIFIED="1631867376791">
+<node TEXT="语言表" ID="ID_386555358" CREATED="1631867465569" MODIFIED="1631867547254"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      不同的语言
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="章节表" FOLDED="true" ID="ID_1627524300" CREATED="1631868250564" MODIFIED="1631876161782">
+<node TEXT="段落表" ID="ID_803111357" CREATED="1631867407646" MODIFIED="1631867415553">
+<node TEXT="句子表" ID="ID_991182461" CREATED="1631867428127" MODIFIED="1631867447069"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="关卡表" ID="ID_204095395" CREATED="1631867320032" MODIFIED="1631867370965">
+<node TEXT="关卡主题" ID="ID_1138405008" CREATED="1631867350928" MODIFIED="1631867361757"/>
+<node TEXT="完成次数" ID="ID_267114994" CREATED="1631867331625" MODIFIED="1631867342149"/>
+</node>
+</node>
+</node>
+</map>
